de11d05479b5bc30354aeebf411f52be3851e5b1
ujjwal patel <newujjwalpatel@gmail.com>
2024-01-09 23:44:08 +0530

    first commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("venv/serviceAccountKey.json")
+cred = credentials.Certificate("/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db=firestore.client()
 
