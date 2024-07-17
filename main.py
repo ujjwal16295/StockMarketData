bbfacdba58385e8fc3e8e389dc52a3f8b54e1320
ujjwal16295 <115917075+ujjwal16295@users.noreply.github.com>
2024-07-17 15:37:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ firebase_config={
   "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
   "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
 }
-cred = credentials.Certificate(firebasse_config)
+cred = credentials.Certificate(firebase_config)
 firebase_admin.initialize_app(cred)
 db=firestore.client()
 
