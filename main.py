8940dafc354708198a66b9c0623344b2b2f6dba7
ujjwal16295 <115917075+ujjwal16295@users.noreply.github.com>
2024-07-17 16:56:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,9 +35,9 @@ db=firestore.client()
 
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=chrome_options)
 
 
