bf920f54b1e38dd331b566dc3c6d57ca4d92d600
ujjwal16295 <115917075+ujjwal16295@users.noreply.github.com>
2024-12-24 20:19:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,23 @@ from firebase_admin import firestore
 import os
 
 # add firebase config here
-# firebase_config=
+firebase_config={
+  "type": os.getenv("TYPE"),
+  "project_id": os.getenv("PROJECT_ID"),
+  "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+  "private_key": os.getenv("PRIVATE_KEY"),
+  "client_email": os.getenv("CLIENT_EMAIL"),
+  "client_id": os.getenv("CLIENT_ID"),
+  "auth_uri":  os.getenv("AUTH_URI"),
+  "token_uri": os.getenv("TOKEN_URI"),
+  "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+  "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+  "universe_domain": os.getenv("UNIVERSE_DOMAIN")
+}
 
 
-cred = credentials.Certificate("serviceAccountKey.json")
+
+cred = credentials.Certificate(firebase_config)
 firebase_admin.initialize_app(cred)
 db=firestore.client()
 
